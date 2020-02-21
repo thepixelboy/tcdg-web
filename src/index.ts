aa09e671ff1c883387e4c57eab7ec10feb45ee34
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-02-21 19:20:56 +0100

    Final User refactor

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ id: 2, name: 'monica duchessne', age: 29 });
+// const user = new User({ id: 2, name: 'monica duchessne', age: 29 });
 
-user.on('save', () => {
-  console.log(user);
-});
+// user.on('save', () => {
+//   console.log(user);
+// });
 
-user.save();
+// user.save();
