be4e6003cafcad26ce3df389538a49ceffc44c54
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-02-21 19:26:28 +0100

    Model wrapup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { User } from './models/User';
 
-// const user = new User({ id: 2, name: 'monica duchessne', age: 29 });
+const user = User.buildUser({ id: 1 });
 
-// user.on('save', () => {
-//   console.log(user);
-// });
+user.on('change', () => {
+  console.log(user);
+});
 
-// user.save();
+user.fetch();
