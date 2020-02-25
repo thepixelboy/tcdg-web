3f02a0cb088f08c793b9d9fffb540ded95c5816d
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-02-25 10:29:54 +0100

    CollectionView implementation and App wrapup

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
-import { UserForm } from './views/UserForm';
-import { User } from './models/User';
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { UserProps, User } from './models/User';
 
-const user = User.buildUser({ name: 'Olivia Dunham', age: 40 });
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
-const root = document.getElementById('root');
+users.on('change', () => {
+  const root = document.getElementById('root');
 
-if (root) {
-  const userForm = new UserForm(root, user);
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
 
-  userForm.render();
-} else {
-  throw new Error('Root element not found');
-}
+users.fetch();
